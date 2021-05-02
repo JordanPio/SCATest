b60da8db4841d98b9f22134f2082e8ac25974023
Jordan Pio <czpjunior@gmail.com>
2021-05-02 18:39:41 +1000

    setup activity steps

--- src/index.js
+++ src/index.js
@@ -9,3 +9,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+// consume feed from URL
+
+const podcastURL = `https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss`
+
+//conver xml to json object
+
+
+// format date to AEST
+
+
+// bonus Round - order data
