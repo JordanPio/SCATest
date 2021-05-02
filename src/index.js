9e99d939a4f3ea4382bf1eabfe8d2fe19bc0aa94
Jordan Pio <czpjunior@gmail.com>
2021-05-02 19:14:39 +1000

    first Challenge completed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const Parser = require('rss-parser')
+const parser = new Parser()
+const {convertISODateToAEST} = require("./utilities/format-iso-date-to-AEST")
+
+
+const podcastURL = `https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss`
+
+app.get('/', async (req, res) => {
+  // res.send('Hello World!')
+  const feed = await getFeedInJson(podcastURL)
+  const FormatedFeedObject = await remodelFeedObj(feed)
+  res.send(FormatedFeedObject)
+
+
 })
 
 app.listen(port, () => {
@@ -14,12 +26,43 @@ app.listen(port, () => {
 
 // consume feed from URL
 
-const podcastURL = `https://www.nasa.gov/rss/dyn/Houston-We-Have-a-Podcast.rss`
 
-//conver xml to json object
+//convert xml to json object
+
+async function getFeedInJson(podcastURL) {
+  try {
+    return await parser.parseURL(podcastURL)
+  } catch (error) {
+    return {}
+  }
+}
 
 
-// format date to AEST
+// getFeedInJson(podcastURL).then(data => console.log(data))
+
+
+// remodel feedObj and formatDate to AEST
+function remodelFeedObj(feed) {
+  // const feed = await getFeedInJson(podcastURL)
+
+  const episodesArr = feed.items.map(epi => {
+    publishedDate = convertISODateToAEST(epi.pubDate)
+    return {title: epi.title, audioUrl: epi.enclosure.url, publishedDate}
+  })
+
+  const last10Episodes = episodesArr.slice(0, 10)
+
+  let newFeedObj = {title: feed.title , description: feed.description , episodes: last10Episodes}
+
+  return newFeedObj
+
+  // console.log(Object.keys(feed))
+  // console.log(newFeedObj)
+}
+
+// remodelFeedObj()
+
+
 
 
 // bonus Round - order data
